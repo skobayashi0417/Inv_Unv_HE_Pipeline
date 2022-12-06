f95dbf139731c96f8f86f01abf3241f351ed668b
skobayashi0417 <soma.kobayashi@stonybrookmedicine.edu>
2022-12-06 13:17:45 -0500

    edit generateJson

--- generateJSON.py
+++ generateJSON.py
@@ -11,7 +11,7 @@ def generate_JSON():
     data['csInference'] = {}
 
     data['directories']['INPUT_WSI_DIR'] = '/data06/shared/skobayashi/downloadVSI/Yang_Prospect2/Scan_composite_wChronic'
-    data['directories']['BASE_DIR'] = '/data10/shared/skobayashi/YANGPROSPECTIVE_fullPipeline_allMice'
+    data['directories']['BASE_DIR'] = '/data05/shared/skobayashi/YANGPROSPECTIVE_fullPipeline_allMice'
 
     data['SlideInfo']['WSI_EXTENSION'] = '.tif'
     data['SlideInfo']['SCALE_FACTOR'] = 8
