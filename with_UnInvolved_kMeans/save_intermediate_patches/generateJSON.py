f6556db1afee38e52b8e39ac982129157689e5a1
skobayashi0417 <soma.kobayashi@stonybrookmedicine.edu>
2024-11-27 18:14:49 -0500

    change paths

--- with_UnInvolved_kMeans/save_intermediate_patches/generateJSON.py
+++ with_UnInvolved_kMeans/save_intermediate_patches/generateJSON.py
@@ -10,8 +10,8 @@ def generate_JSON():
     data['mouseModelInference'] = {}
     data['csInference'] = {}
 
-    data['directories']['INPUT_WSI_DIR'] = '/data06/shared/skobayashi/paraffin40Xbigtif'
-    data['directories']['BASE_DIR'] = '/data02/shared/skobayashi/HE_Inv_UnInv_2mousecheck'
+    data['directories']['INPUT_WSI_DIR'] = '/data02/shared/skobayashi/downloadVSI/Yang_Prospect2/Scan_composite_wChronic_reLabeled'
+    data['directories']['BASE_DIR'] = '/data01/shared/skobayashi/HE_Inv_UnInv_sortUnInvKmeans'
 
     data['SlideInfo']['WSI_EXTENSION'] = '.tif'
     data['SlideInfo']['SCALE_FACTOR'] = 8
